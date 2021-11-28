5a0523ae56d914b80bcd63c0070a88af17e022ee
jowanajav <72789389+jowanajav@users.noreply.github.com>
2021-11-28 17:33:17 +0900

    removed target="_blank" on links

--- src/Footer.js
+++ src/Footer.js
@@ -5,13 +5,8 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <a href="https://github.com/jowanajav/weather-react" target="_blank">
-        Open-source code
-      </a>{" "}
-      by{" "}
-      <a href="https://boring-bartik-a4ead2.netlify.app" target="_blank">
-        Jhoanna Javier
-      </a>
+      <a href="https://github.com/jowanajav/weather-react">Open-source code</a>{" "}
+      by <a href="https://boring-bartik-a4ead2.netlify.app">Jhoanna Javier</a>
     </div>
   );
 }
